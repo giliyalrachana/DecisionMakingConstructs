13dab6dacb1ddc7e0e14981863b498d8eeed4b0c
Rachana <giliyalrachana@gmail.com>
2021-09-21 13:28:06 +0530

    source code added to IfElseDemo.cs

--- DecisionMakingConstruct/DecisionMakingConstruct/IfElseDemo.cs
+++ DecisionMakingConstruct/DecisionMakingConstruct/IfElseDemo.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DecisionMakingConstruct
 {
+    // This program demo. how to write else block
     class IfElseDemo
     {
+        static void Main()
+        {
+            Console.Write("Enter Annual Income:");
+            double annualIncome = Convert.ToDouble(Console.ReadLine());
+            if (annualIncome >= 280000) ;
+            {
+                Console.WriteLine("You are liable to pay I.T");
+            }
+            else
+            {
+                Console.WriteLine("You are not liable to pay I.T");
+            }
+            Console.ReadLine();
+        }
     }
 }
+
+    
+
