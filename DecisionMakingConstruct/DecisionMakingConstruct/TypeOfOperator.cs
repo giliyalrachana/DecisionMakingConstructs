d8849c83b5ff3538f0d5d4b4fab1f5a0295592ed
giliyalrachana <91076794+giliyalrachana@users.noreply.github.com>
2021-09-21 13:43:27 +0530

    IfElseIf is added
    new class file is added

---
+++ DecisionMakingConstruct/DecisionMakingConstruct/TypeOfOperator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStartedWithCSharp
+{
+    class TypeOfOperator
+    {
+        static void Main()
+        {
+            // typeof is used to find the managed type of type at run time
+            Console.WriteLine(typeof(int));
+            Console.WriteLine(typeof(string));
+            Console.ReadLine();
+ 
+        }
+    }
+}
