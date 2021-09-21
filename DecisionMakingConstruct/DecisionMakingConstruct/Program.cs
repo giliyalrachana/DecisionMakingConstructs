8ac67ffd70ea387d37108e4bbbdf5e1bd0b11b3f
Rachana <giliyalrachana@gmail.com>
2021-09-21 12:55:50 +0530

    source code added to program.cs file

--- DecisionMakingConstruct/DecisionMakingConstruct/Program.cs
+++ DecisionMakingConstruct/DecisionMakingConstruct/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DecisionMakingConstruct
 {
     class Program
     {
+        // This program demo how to write if statement
         static void Main(string[] args)
         {
+            Console.Write("Enter Annual Income:");
+            double annualIncome = Convert.ToDouble(Console.ReadLine());
+            if (annualIncome >= 28000000);
+            {
+                Console.WriteLine("You are liable to pay I.T");
+            }
+                Console.ReadLine();
+            }
         }
     }
-}
+
