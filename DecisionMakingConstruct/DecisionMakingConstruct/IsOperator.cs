720c3ef1aa3b7041638b2778990781fada156a66
giliyalrachana <91076794+giliyalrachana@users.noreply.github.com>
2021-09-21 12:39:37 +0530

    new class file is added
    Is Operator

---
+++ DecisionMakingConstruct/DecisionMakingConstruct/IsOperator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GettingStartedWithCSharp
+{
+    class IsAOperator
+    {
+        static void Main()
+        {
+            // Is operator returns true if the given object is of the same type
+            C c = new A();
+            Console.WriteLine (c is D);
+            D d = new B();
+            Console.WriteLine(d is C);
+            Console.ReadLine();
+
+        }
+    }
+    class C
+    {
+
+    }
+    class D : C
+    {
+
+    }
+}
